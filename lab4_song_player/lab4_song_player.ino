5cbb151592c79e8e54b67204b8788aba24769d27
mziz <milda_zizyte@brown.edu>
2025-09-27 18:18:23 -0400

    Update lab4_song_player.ino

--- lab4_song_player/lab4_song_player.ino
+++ lab4_song_player/lab4_song_player.ino
@@ -12,7 +12,7 @@ const int CLOCKFREQ = ;
 // const int OUT_PORT = ;
 // const int OUT_PIN = ;
 
-const String song = "spooky:d=4,o=6,b=127:8c,f,8a,f,8c,b5,2g,8f,e,8g,e,8e5,a5,2f,8c,f,8a,f,8c,b5,2g,8f,e,8c,d,8e,1f,8c,8d,8e,8f,1p,8d,8e,8f_"
+const String song = "spooky:d=4,o=6,b=127:8c,f,8a,f,8c,b5,2g,8f,e,8g,e,8e5,a5,2f,8c,f,8a,f,8c,b5,2g,8f,e,8c,d,8e,1f,8c,8d,8e,8f,1p,8d,8e,8f_";
 //const String song = "Short:d=16,o=5,b=140:b,8p,b,b,2b";
 // TODO step 4
 //const String song = "Pinkpanth:d=4,o=6,b=160:8d_5,8e5,2p,8f_5,8g5,2p,8d_5,8e5,16p,8f_5,8g5,16p,8c,8b5,16p,8d_5,8e5,16p,8b5,2a_5,2p,16a5,16g5,16e5,16d5,2e5";
